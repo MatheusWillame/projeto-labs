b85a9eb61fcd775223b78095f82c3f255fcf3303
Matheus Willame <matheus.willame@gmail.com>
2022-04-11 17:08:30 -0300

    arquivos reactJs

---
+++ react-library/src/components/stencil-generated/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'stencil-library';
+
+import { defineCustomElements } from 'stencil-library/loader';
+
+defineCustomElements();
+export const AppTodo = /*@__PURE__*/createReactComponent<JSX.AppTodo, HTMLAppTodoElement>('app-todo');
